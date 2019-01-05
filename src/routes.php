c64770bdfc1975c21c05c246389c8be1fe48798c
Sébastien Lorrain <sldeveloperand@gmail.com>
2019-01-05 19:31:38 +0100

    fix: routes

--- src/routes.php
+++ src/routes.php
@@ -5,32 +5,36 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$app->get('/', function (Request $request, Response $response, array $args) {
+
+$app->group('/cronwa/', function (App $app) {
+
+    $app->get('', function (Request $request, Response $response, array $args) {
 
 
-    return $this->view->render($response, 'crontabs.html.twig', ['crontabs' => $this->crontabs->getCronTabs()]);
-});
+        return $this->view->render($response, 'crontabs.html.twig', ['crontabs' => $this->crontabs->getCronTabs()]);
+    });
 
-$app->get('/edit/{tab}/{job}', function (Request $request, Response $response, array $args) {
-    $tab = $args['tab'];
-    $job = $args['job'];
+    $app->get('edit/{tab}/{job}', function (Request $request, Response $response, array $args) {
+        $tab = $args['tab'];
+        $job = $args['job'];
 
-    /** @var CronTab $cronTab */
-    $cronTab = $this->crontabs->getCronTab($tab);
-    $job = $cronTab->getJob($job);
+        /** @var CronTab $cronTab */
+        $cronTab = $this->crontabs->getCronTab($tab);
+        $job = $cronTab->getJob($job);
 
-    return $this->view->render($response, 'crontab-form.html.twig', ['cronTab' => $cronTab, 'job' => $job]);
-});
+        return $this->view->render($response, 'crontab-form.html.twig', ['cronTab' => $cronTab, 'job' => $job]);
+    });
 
-$app->map(['POST', 'DELETE'], '/{name}', function (Request $request, Response $response, array $args) {
+    $app->map(['POST', 'DELETE'], '/{name}', function (Request $request, Response $response, array $args) {
 
-    if ($request->isPost()) {
-        $body = $request->getParsedBody();
+        if ($request->isPost()) {
+            $body = $request->getParsedBody();
 
-        return $this->view->render($response, 'crontab-edit.html.twig', ['name' => $body]);
-    }
+            return $this->view->render($response, 'crontab-edit.html.twig', ['name' => $body]);
+        }
 
-    return $this->view->render($response, 'crontabs.html.twig', ['crontabs' => $this->crontabs->getCronTabs()]);
+        return $this->view->render($response, 'crontabs.html.twig', ['crontabs' => $this->crontabs->getCronTabs()]);
+    });
 });
 
 
